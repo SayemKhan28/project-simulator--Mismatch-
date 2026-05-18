e9b9016f3449548e5ac271fb70c88a619fbcad14
Bhowmick Nishi <nishibhowmick7@gmail.com>
2026-05-18 09:25:11 +0600

    Create Card3.cpp

---
+++ Card3.cpp
@@ -0,0 +1,32 @@
+NFR-Admin - The system should ensure usability so that the interface is simple, intuitive, and easy for the Admin to understand and operate without requiring extensive training.
+Usability
+
+[Admin]
+
+ 
+
+Clear navigation menu
+
+Consistent layout across pages (mandatory UI structure)
+
+Readable labels and text
+
+Buttons with clear actions
+
+Error/help messages
+
+Tooltip or guidance hints (optional)
+
+As an admin, I want to use the system easily so that I can perform tasks without confusion or difficulty.
+
+ 
+
+When the interface is unclear and navigation is difficult, the system should result in poor usability and user confusion.
+
+When the interface is unclear but navigation is easy, the system should still allow task completion with minor confusion.
+
+When the interface is clear but navigation is difficult, the system should show usability issues in moving between features.
+
+When the interface is clear and navigation is easy, the system should provide a smooth and user-friendly experience.
+
+
